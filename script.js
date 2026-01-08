f29ae3851c346747e5301a33dce1c50ae5246e8d
Pau Muan Khai <amuan@The-Beast.local>
2026-01-08 23:22:01 +0800

    this is today's progreess, I added the logic for winning conditions, that's all

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ let gameBoard = ["","",""
                         ,"","",""];
 
 let playerOne = "X";
-let playerTwo = "0";
+let playerTwo = "O";
 
-gameBoard[0]= playerOne;
-gameBoard[4]= playerOne;
-gameBoard[2]= playerOne;
 
-if (gameBoard[0] == "X" && gameBoard[1] == "X" && gameBoard[2] == "X") {
+
+gameBoard[1]= playerOne;
+gameBoard[6]= playerOne;
+gameBoard[8]= playerOne;
+
+if (gameBoard[0] == "X" && gameBoard[1] == "X" && gameBoard[2] == "X"
+    || gameBoard[3] == "X" && gameBoard[4] == "X" && gameBoard[5] == "X"
+    || gameBoard[6] == "X" && gameBoard[7] == "X" && gameBoard[8] == "X"
+    || gameBoard[0] == "X" && gameBoard[3] == "X" && gameBoard[6] == "X"
+    || gameBoard[1] == "X" && gameBoard[4] == "X" && gameBoard[7] == "X"
+    || gameBoard[2] == "X" && gameBoard[5] == "X" && gameBoard[8] == "X"
+    || gameBoard[0] == "X" && gameBoard[4] == "X" && gameBoard[8] == "X"
+    || gameBoard[2] == "X" && gameBoard[4] == "X" && gameBoard[6] == "X") {
     console.log("Player 1 wins");
+}else{
+    console.log("nothing");
 }
-
